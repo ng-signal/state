03042ce7fd9596a9f61c5fa4018975c041618089
Brian Pilati <brianpilati@gmail.com>
2025-11-14 13:49:12 -0600

    Added new files for the chrome plugin

--- projects/dev-tools/src/lib/chrome-plugin/ngvault-devtools.js
+++
@@ -1,5 +0,0 @@
-console.log('[ngVault DevTools] DevTools entry loaded.');
-
-chrome.devtools.panels.create('ngVault', 'icons/ngvault-128.png', 'panel.html', function (panel) {
-  console.log('[ngVault DevTools] Panel created.');
-});
